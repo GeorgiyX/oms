049e98d0fde3e514b8892fefa7e503280aee1e30
Sedoykin Georgiy <gsedoykin@ozon.ru>
2023-04-04 16:40:58 +0300

    Refactor kafka pkg

--- libs/kafka/consumer/consumer_group.go
+++ libs/kafka/consumer/consumer_group.go
@@ -11,48 +11,10 @@ import (
 
 type MessageHandler func(ctx context.Context, message *sarama.ConsumerMessage) error
 
-type consumer struct {
-	ready     chan bool
-	skipOnErr bool
-	handler   MessageHandler
-}
+var _ ConsumerGroup = (*consumerGroup)(nil)
 
-// Setup is run at the beginning of a new session, before ConsumeClaim
-func (c *consumer) Setup(sarama.ConsumerGroupSession) error {
-	close(c.ready) // Mark the consumer as ready
-	return nil
-}
-
-// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
-func (c *consumer) Cleanup(sarama.ConsumerGroupSession) error {
-	return nil
-}
-
-// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
-func (c *consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	for {
-		select {
-		case message, ok := <-claim.Messages():
-			if !ok {
-				return nil
-			}
-			logMessageClaim(message)
-
-			err := c.handler(session.Context(), message)
-			if err != nil && !c.skipOnErr { //
-				return err // stop consumer group session, without commit
-			}
-
-			session.MarkMessage(message, "")
-
-		case <-session.Context().Done():
-			return nil // stop session, go re-balance now
-		}
-	}
-}
-
-func logMessageClaim(message *sarama.ConsumerMessage) {
-	log.Printf("Message claimed: timestamp = %v, key = %s, topic = %s", message.Timestamp, string(message.Key), message.Topic)
+type ConsumerGroup interface {
+	Cancel() error
 }
 
 type consumerGroup struct {
@@ -69,6 +31,7 @@ type ConfigConsumer struct {
 	Group         string
 	Brokers       []string
 	Handler       MessageHandler
+	SkipOnErr     bool
 }
 
 func NewConsumerGroup(cfg ConfigConsumer) (*consumerGroup, error) {
@@ -79,7 +42,7 @@ func NewConsumerGroup(cfg ConfigConsumer) (*consumerGroup, error) {
 
 	c := consumer{
 		ready:     make(chan bool),
-		skipOnErr: false,
+		skipOnErr: cfg.SkipOnErr,
 		handler:   wrapNil(cfg.Handler),
 	}
 
